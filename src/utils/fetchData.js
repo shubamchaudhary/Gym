1eb1d0175a73247f3329c4f606d05a3b7512294f
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2023-07-30 03:18:25 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'cd22fbe4ffmshdc1583db0f036fbp1a6b08jsn02c89c811da6',
   },
 };
 
